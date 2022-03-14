1f761844af2861d6d78acb60f0e8a69344744b4f
bskhajone <bhushankhajone@gmail.com>
2022-03-14 23:18:56 +0530

    Method overloading quick description.

--- src/practices/Overloading.java
+++ src/practices/Overloading.java
@@ -3,17 +3,22 @@ package practices;
 public class Overloading {
     public static void main(String[] a) {
         Area area = new Area();
-        area.calculateArea(5.6f);
-        area.calculateArea(7f, 5.6f);
+        area.calculateArea(5.6f); //Java knows which method to call on compile time itself.
+        area.calculateArea(7f, 5.6f); //Hence Method overloading is also called compile-time polymorphism
     }
 }
 
-class Area {
-    void calculateArea(float radius) {
+class Area
+{
+    //Following two methods have same name but different signatures(difference in parameter)
+    //This is method overloading
+    void calculateArea(float radius)
+    {
         System.out.println("Area of circle is: " + (3.14 * radius * radius));
     }
 
-    void calculateArea(float length, float bredth){
+    void calculateArea(float length, float bredth)
+    {
         System.out.println("Area of reactangle is: " + (length*bredth));
     }
 }
