4ece6408291bdc2cfc92965185797b2951aed67a
bhushankhajone <22050802+bhushankhajone@users.noreply.github.com>
2022-03-21 15:37:48 +0530

    Exercise: Currency Converter using java

---
+++ src/practices/CurrencyConverter.java
@@ -0,0 +1,8 @@
+/*
+Problem Statement:
+Write a program to convert currency value from one currency to another.
+At the beginning program should show list of currencies for base currency selection from user.
+On valid base currency selection program will show list to currencies again to select target currency.
+If user select target currenct same as a base currency, program will print message "You have chosen same base currency and target currency and does not need conversion".
+If both inputs are different, program should ask for base currency units and should print conversion value.
+*/
